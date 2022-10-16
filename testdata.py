578f362d658aa146ef2ab68404162414c5144a78
Rach <pradhan.rach4@gmail.com>
2022-10-16 14:43:18 +0800

    added coordinate translators

--- testdata.py
+++ testdata.py
@@ -6,22 +6,23 @@ import streamlit as st
 
 st.title("Test Data")
 #Get the current date time
-output_date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+output_date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%z")
 val = "YYYY-MM-DD[T]HH:mm:ss"
 val = "2022-10-06T10:51:34.765Z"
 print(str(output_date))
 # Get the data from the API
-data = req.get("https://api.data.gov.sg/v1/transport/carpark-availability?date_time=" + val ).json()
+st.write(output_date)
+data = req.get("https://api.data.gov.sg/v1/transport/carpark-availability?date_time=" + str(output_date) ).json()
 
 # st.write(data)
 
 
 
 # To convert data to a dataframe
-df = pd.DataFrame(data['items'][0]['carpark_data'])
-# To read items inside carpark_info dataframe
-print(df)
+# df = pd.DataFrame(data['items'][0]['carpark_data'])
+# # To read items inside carpark_info dataframe
+# print(df)
 
-st.write(df)
+st.write(data)
 
 
